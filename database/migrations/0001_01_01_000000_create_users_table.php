7e9a11d07a4cd3b9e3dcc74d7cde687096352e4c
PabloTenorio <pjtesc31@gmail.com>
2024-11-13 10:53:04 -0600

    Migraciones y Modelos actualizados (Fase 2)

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedBigInteger('rol');
+            $table->foreign('rol')->references('id')->on('roles')->default(2);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
---
+++ database/migrations/2024_10_22_214357_create_catalogos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('catalogos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('creador');
+            $table->foreign('creador')->references('id')->on('users');
+            $table->string('name');
+            $table->string('color');
+            $table->unsignedBigInteger('categoria');
+            $table->foreign('categoria')->references('id')->on('categorias');
+            $table->string('material');
+            $table->string('descripción');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('catalogos');
+    }
+};
